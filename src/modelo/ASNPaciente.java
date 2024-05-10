93346224487f77c1bc934272140a3fa878bae103
sebas <sebas@BT>
2024-05-10 17:54:15 +0200

    Segundo commit tarea consultorio

--- src/modelo/ASNPaciente.java
+++ src/modelo/ASNPaciente.java
@@ -4,10 +4,146 @@
  */
 package modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author sebas
  */
 public class ASNPaciente {
+    
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private Date fechaNacimiento;
+    private int telefono;
+    private String email;
+    private int cp;
+    private String sexo;
+    private String tabaquismo;
+    private String consumoAlcohol;
+    private String antecedentesSalud;
+    private String datosSaludGeneral;
+    private Date fechaRegistro;
+
+    public ASNPaciente(String dni, String nombre, String apellidos, Date fechaNacimiento, int telefono, String email, int cp, String sexo, String tabaquismo, String consumoAlcohol, String antecedentesSalud, String datosSaludGeneral, Date fechaRegistro) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.email = email;
+        this.cp = cp;
+        this.sexo = sexo;
+        this.tabaquismo = tabaquismo;
+        this.consumoAlcohol = consumoAlcohol;
+        this.antecedentesSalud = antecedentesSalud;
+        this.datosSaludGeneral = datosSaludGeneral;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTabaquismo() {
+        return tabaquismo;
+    }
+
+    public void setTabaquismo(String tabaquismo) {
+        this.tabaquismo = tabaquismo;
+    }
+
+    public String getConsumoAlcohol() {
+        return consumoAlcohol;
+    }
+
+    public void setConsumoAlcohol(String consumoAlcohol) {
+        this.consumoAlcohol = consumoAlcohol;
+    }
+
+    public String getAntecedentesSalud() {
+        return antecedentesSalud;
+    }
+
+    public void setAntecedentesSalud(String antecedentesSalud) {
+        this.antecedentesSalud = antecedentesSalud;
+    }
+
+    public String getDatosSaludGeneral() {
+        return datosSaludGeneral;
+    }
+
+    public void setDatosSaludGeneral(String datosSaludGeneral) {
+        this.datosSaludGeneral = datosSaludGeneral;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
     
 }
--- src/modelo/ASNPersonal.java
+++ src/modelo/ASNPersonal.java
@@ -10,4 +10,78 @@ package modelo;
  */
 public class ASNPersonal {
     
+    private int numero_colegiado;
+    private String nomrbe;
+    private String apellidos;
+    private int telefono;
+    private String usuario;
+    private String contrasenya;
+    private String tipo;
+
+    public ASNPersonal(int numero_colegiado, String nomrbe, String apellidos, int telefono, String usuario, String contrasenya, String tipo) {
+        this.numero_colegiado = numero_colegiado;
+        this.nomrbe = nomrbe;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.usuario = usuario;
+        this.contrasenya = contrasenya;
+        this.tipo = tipo;
+    }
+
+    public int getNumero_colegiado() {
+        return numero_colegiado;
+    }
+
+    public void setNumero_colegiado(int numero_colegiado) {
+        this.numero_colegiado = numero_colegiado;
+    }
+
+    public String getNomrbe() {
+        return nomrbe;
+    }
+
+    public void setNomrbe(String nomrbe) {
+        this.nomrbe = nomrbe;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
 }
